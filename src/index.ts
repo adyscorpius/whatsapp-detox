efafde94e0560ebf115aab1852b416321630fa05
Adit Sharda <mailme@aditsharda.com>
2020-06-11 01:57:42 +0530

    Fix process not ending because process.exit not called.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
 process.env.NTBA_FIX_319 = "1";
 import MyWhatsapp from "./my-whatsapp";
 import Config from "./Config";
+import readline from "readline";
 const main = new MyWhatsapp(Config.type); // CHANGE TO CONSOLE IF YOU DON"T WANT TO BE NOTIFIED ON TELEGRAM.
 
 (async () => {
   try {
-    process.on("beforeExit", async () => await main.end());
-    process.on("SIGKILL", (t) => {
+    process.on("SIGINT", (t) => {
       console.log(t);
     });
+
+    if (process.platform === "win32") {
+      var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.on("SIGINT", function () {
+        process.emit("disconnect");
+      });
+    }
+
+    process.on("disconnect", async function () {
+      //graceful shutdown
+      await main.end();
+      process.exit();
+    });
+
     await main.init();
   } catch (e) {
     console.log("Master Error", e);
